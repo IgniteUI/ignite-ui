998429e549a829d6a0a200d58693008fe7800da0
ig-ealbert <ealbert@infragistics.com>
2016-07-06 11:01:08 -0400

    Added new zoombar l10n entry

--- src/js/modules/i18n/infragistics.ui.zoombar-bg.js
+++ src/js/modules/i18n/infragistics.ui.zoombar-bg.js
@@ -19,6 +19,7 @@
 	        locale: {
 	            zoombarTargetNotSpecified: "igZoombar изисква валидна цел, към която да се прикрепи!",
 	            zoombarTypeNotSupported: "Видът на компонента, към който igZoombar се опитва да се прикрепи не се поддържа!",
+				zoombarProviderNotRecognized: "igZoombar could not recognize the provider specified. If you are using a custom one, please ensure that you are passing the name of an existing Class in the $.ig namespace or an instance of one.",
 	            optionChangeNotSupported: "Промяната на следната опция след инициализация на igZoombar не се поддържа:"
 		    }
 	    });
