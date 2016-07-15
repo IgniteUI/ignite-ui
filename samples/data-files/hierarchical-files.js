87ced2ea45d42094ff27b830cf826e6654130328
Todor Paskalev <todor.paskalev@gmail.com>
2016-07-15 17:03:23 +0300

    Adding OSS samples
    #22

---
+++ samples/data-files/hierarchical-files.js
@@ -0,0 +1,75 @@
+﻿var files = [
+    {
+    	Text: "Computer", Value: "Folder", ImageUrl: "../../images/samples/tree/computer.png", Folder: [
+          {
+          	Text: "Music", Value: "Folder", ImageUrl: "../../images/samples/tree/book.png", Folder: [
+			  {
+			  	Text: "Y.Malmsteen", Value: "Folder", ImageUrl: "../../images/samples/tree/book.png", Folder: [{
+			  		Text: "Making Love", Value: "File", ImageUrl: "../../images/samples/tree/music.png", Folder: ""
+			  	}, { Text: "Rising Force", Value: "File", ImageUrl: "../../images/samples/tree/music.png", Folder: "" },
+				{ Text: "Fire and Ice", Value: "File", ImageUrl: "../../images/samples/tree/music.png", Folder: "" }
+			  	]
+			  },
+			  {
+			  	Text: "WhiteSnake", Value: "Folder", ImageUrl: "../../images/samples/tree/book.png", Folder: [
+				  { Text: "Trouble", Value: "File", ImageUrl: "../../images/samples/tree/music.png", Folder: "" },
+				  { Text: "Bad Boys", Value: "File", ImageUrl: "../../images/samples/tree/music.png", Folder: "" },
+				  { Text: "Is This Love", Value: "File", ImageUrl: "../../images/samples/tree/music.png", Folder: "" }
+			  	]
+			  },
+			  {
+			  	Text: "AC/DC", Value: "Folder", ImageUrl: "../../images/samples/tree/book.png", Folder: [
+				  { Text: "ThunderStruck", Value: "File", ImageUrl: "../../images/samples/tree/music.png", Folder: "" },
+				  { Text: "T.N.T.", Value: "File", ImageUrl: "../../images/samples/tree/music.png", Folder: "" },
+				  { Text: "The Jack", Value: "File", ImageUrl: "../../images/samples/tree/music.png", Folder: "" }
+			  	]
+			  },
+			  {
+			  	Text: "Rock", Value: "Folder", ImageUrl: "../../images/samples/tree/book.png", Folder: [{
+			  		Text: "Bon Jovi - Always", Value: "File", ImageUrl: "../../images/samples/tree/music.png", Folder: ""
+			  	}]
+			  }]
+          },
+          {
+          	Text: "My Documents", Value: "Folder", ImageUrl: "../../images/samples/tree/documents-folder.png", Folder: [
+			  {
+			  	Text: "2009", Value: "Folder", ImageUrl: "../../images/samples/tree/documents-folder.png", Folder: [
+				  { Text: "Month Report", Value: "File", ImageUrl: "../../images/samples/tree/documents.png", Folder: "" },
+				  { Text: "Year Report", Value: "File", ImageUrl: "../../images/samples/tree/documents.png", Folder: "" }
+			  	]
+			  },
+			  {
+			  	Text: "2010", Value: "Folder", ImageUrl: "../../images/samples/tree/documents-folder.png", Folder: [
+				  { Text: "Month Report", Value: "File", ImageUrl: "../../images/samples/tree/documents.png", Folder: "" },
+				  { Text: "Year Report", Value: "File", ImageUrl: "../../images/samples/tree/documents.png", Folder: "" }
+			  	]
+			  }]
+          },
+          {
+          	Text: "Pictures", Value: "Folder", ImageUrl: "../../images/samples/tree/coins.png", Folder: [
+			  {
+			  	Text: "BirthDay2009", Value: "Folder", ImageUrl: "../../images/samples/tree/coins.png", Folder: [
+				  { Text: "Picture1", Value: "File", ImageUrl: "../../images/samples/tree/coins_add.png", Folder: "" },
+				  { Text: "Picture2", Value: "File", ImageUrl: "../../images/samples/tree/coins_add.png", Folder: "" },
+				  { Text: "Picture3", Value: "File", ImageUrl: "../../images/samples/tree/coins_add.png", Folder: "" },
+				  { Text: "Picture4", Value: "File", ImageUrl: "../../images/samples/tree/coins_add.png", Folder: "" }
+			  	]
+			  },
+			  {
+			  	Text: "BirthDay2010", Value: "Folder", ImageUrl: "../../images/samples/tree/coins.png", Folder: [
+				  { Text: "Picture1", Value: "File", ImageUrl: "../../images/samples/tree/coins_add.png", Folder: "" },
+				  { Text: "Picture2", Value: "File", ImageUrl: "../../images/samples/tree/coins_add.png", Folder: "" },
+				  { Text: "Picture3", Value: "File", ImageUrl: "../../images/samples/tree/coins_add.png", Folder: "" }
+			  	]
+			  }]
+          },
+          {
+          	Text: "Network", Value: "Folder", ImageUrl: "../../images/samples/tree/door.png", Folder: [
+			  { Text: "Archive", Value: "Folder", ImageUrl: "../../images/samples/tree/door_in.png", Folder: "" },
+			  { Text: "BackUp", Value: "Folder", ImageUrl: "../../images/samples/tree/door_in.png", Folder: "" },
+			  { Text: "FTP", Value: "Folder", ImageUrl: "../../images/samples/tree/door_in.png", Folder: "" }
+          	]
+          },
+          { Text: "Deleted", Value: "Folder", ImageUrl: "../../images/samples/tree/bin_empty.png", Folder: "" }
+    	]
+    }];
