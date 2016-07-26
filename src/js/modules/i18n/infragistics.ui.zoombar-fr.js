4bff21eb76d908d33b9391bc35becae462bb4cc3
Stamen Stoychev <chronos.stz@gmail.com>
2016-07-26 13:53:14 +0300

    Provider API change.

--- src/js/modules/i18n/infragistics.ui.zoombar-fr.js
+++ src/js/modules/i18n/infragistics.ui.zoombar-fr.js
@@ -16,11 +16,11 @@ if (!$.ig.Zoombar) {
 
 	$.extend($.ig.Zoombar, {
 
-	    locale: {
-	        zoombarTargetNotSpecified: "igZoombar a besoin d'une cible valide à laquelle s'attacher.",
-	        zoombarTypeNotSupported: "Le type de widget auquel la barre de zoom tente de s'attacher n'est pas pris en charge.",
-			zoombarProviderNotRecognized: "igZoombar could not recognize the provider specified. If you are using a custom one, please ensure that you are passing the name of an existing Class in the $.ig namespace or an instance of one.",
-	        optionChangeNotSupported: "La modification de l'option suivante après la création de igZoombar n'est pas prise en charge :"
+		locale: {
+			zoombarTargetNotSpecified: "igZoombar a besoin d'une cible valide à laquelle s'attacher.",
+			zoombarTypeNotSupported: "Le type de widget auquel la barre de zoom tente de s'attacher n'est pas pris en charge.",
+			zoombarProviderNotRecognized: "igZoombar could not initialize a provider from the class specified or the value passed is not a class.",
+			optionChangeNotSupported: "La modification de l'option suivante après la création de igZoombar n'est pas prise en charge :"
 		}
 	});
 
