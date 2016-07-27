7270ea54313030b33538ca90fb04b28e01c81df0
skrustev <striker18@mail.bg>
2016-07-27 14:21:02 +0300

    Add more tests regarding thumb dragging and element resizing.

--- tests/unit/scroll/simInteractions.js
+++ tests/unit/scroll/simInteractions.js
@@ -2,7 +2,7 @@
 	$.ig.TestUtil = $.ig.TestUtil || {};
 	
 
-	$.ig.TestUtil.simulateSingleClick = function (node) {
+	$.ig.TestUtil.simulateSingleClick = function (node, options) {
 		var evtMouseOver = $.Event("mouseover"),
 			evtMouseDown = $.Event("mousedown"),
 			evtMouseUp = $.Event("mouseup")
@@ -13,13 +13,23 @@
 		evtMouseUp.target = node[0];
 		evtClick.target = node[0];
 		
+		for( var key in options ) {
+			if(!options.hasOwnProperty(key)) continue;
+			
+			var item = options[key];
+			evtMouseOver[key] = item;
+			evtMouseDown[key] = item;
+			evtMouseUp[key] = item;
+			evtClick[key] = item;	
+		}
+
 		node.trigger(evtMouseOver);
 		node.trigger(evtMouseDown);
 		node.trigger(evtMouseUp);
 		node.trigger(evtClick);
 	};
 
-	$.ig.TestUtil.simulateClickAndHold = function (node, holdTime) {
+	$.ig.TestUtil.simulateClickAndHold = function (node, holdTime, options) {
 		var evtMouseOver = $.Event("mouseover"),
 			evtMouseDown = $.Event("mousedown"),
 			evtMouseUp = $.Event("mouseup")
@@ -29,6 +39,16 @@
 		evtMouseDown.target = node[0];
 		evtMouseUp.target = node[0];
 		evtClick.target = node[0];
+		
+		for( var key in options ) {
+			if(!options.hasOwnProperty(key)) continue;
+			
+			var item = options[key];
+			evtMouseOver[key] = item;
+			evtMouseDown[key] = item;
+			evtMouseUp[key] = item;
+			evtClick[key] = item;	
+		}
 		
 		node.trigger(evtMouseOver);
 		node.trigger(evtMouseDown);
@@ -38,4 +58,69 @@
 			node.trigger(evtClick);
 		}, holdTime);
 	};
+	
+	$.ig.TestUtil.simulateClickDragRelese = function (node, offsetX, offsetY, timeDrag) {
+		var evtMouseOver = $.Event("mouseover"),
+			evtMouseDown = $.Event("mousedown"),
+			evtMouseMove = $.Event("mousemove"),			
+			evtMouseUp = $.Event("mouseup");
+			nodeCenter = getCenter(node),
+			curOffsetX = 0,
+			curOffsetY = 0,
+			stepTime = 0,
+			animationID = 0;
+		
+
+		evtMouseOver.target = node[0];		
+		evtMouseMove.target = node[0];
+		evtMouseDown.target = node[0];
+		evtMouseUp.target = node[0];	
+
+		evtMouseDown.pageX = nodeCenter.x;
+		evtMouseDown.pageY = nodeCenter.y;
+
+		node.trigger(evtMouseOver);
+		node.trigger(evtMouseDown);
+		
+		if(offsetX > offsetY) {
+			stepTime = timeDrag / offsetX;
+		} else {
+			stepTime = timeDrag / offsetY;
+		}
+		
+		function moveStep () {
+			if (curOffsetX < offsetX + 1) {
+				curOffsetX++;
+			}
+			
+			if (curOffsetY < offsetY + 1) {
+				curOffsetY++;
+			}
+			
+			evtMouseMove.pageX = curOffsetX;
+			evtMouseMove.pageY = curOffsetY;
+			node.trigger(evtMouseMove);
+			
+			if(curOffsetX < offsetX + 1 || curOffsetY < offsetY + 1) {
+				animationID = requestAnimationFrame(moveStep);
+			} else {
+				cancelAnimationFrame(animationID);
+				node.trigger(evtMouseUp);
+				return;
+			}
+		};
+		
+		animationID = requestAnimationFrame(moveStep);
+	};
+	
+	function getCenter(node) {		
+		node = $(node);
+		
+		var doc = $(node.ownerDocument),
+			offset = node.offset(),
+			centerX = offset.left + node.outerWidth() / 2 - doc.scrollLeft();
+			centerY = offset.top + node.outerHeight() / 2 - doc.scrollTop();
+		
+		return { x: centerX, y: centerY }
+	}
 })(jQuery);
