b55231f4d06e944bb7f2fdf0257e25178466328f
AMarinov <amarinov@infragistics.com>
2016-07-29 12:05:51 +0300

    Fixing Uglify issues with local files

--- build/packages/combined-files.js
+++ build/packages/combined-files.js
@@ -31,69 +31,84 @@ function buildLocaleMergePairs (locale) {
 }
 
 module.exports = {
-    "options": {
+    "uglify": {
+        options: {
+            expand: true,
+            cwd: 'dist/'
+        },
+        uglifyfiles: {
+            src: ['**/src/js/**/*.js', '!js/modules/i18n/**/*.js', '!**/*-lite.js'],
+            dest: 'dist/',
+            expand: true,
+            options: {
+                // use the general "keep some" regexp  
+                "preserveComments": /(?:^!|@(?:license|preserve|cc_on))/,
+                "compress": false,
+                "mangle": false,
+                "ASCIIOnly": true
+            }
+        },
+        uglifylocale: {
+            options: {
+                src: ['js/modules/i18n/**/*.js'],
+                dest: 'dist/js/modules/i18n/',
 
-        // use the general "keep some" regexp  
-        "preserveComments": /(?:^!|@(?:license|preserve|cc_on))/,
-        "compress": false,
-        "mangle": false
-    },
-    "combine": {
-        "files": {
-            "./dist/js/infragistics.core-lite.js": [
-                "./dist/js/i18n/infragistics-en.js",
-                "./dist/js/modules/infragistics.util.js",
-                "./dist/js/modules/infragistics.dataSource.js",
-                "./dist/js/modules/infragistics.templating.js",
-                "./dist/js/modules/infragistics.ui.shared.js",
-                "./dist/js/modules/infragistics.ui.scroll.js"
-            ],
-            "./dist/js/infragistics.lob-lite.js": [
-                "./dist/js/modules/infragistics.ui.combo.js",
-                "./dist/js/modules/infragistics.ui.dialog.js",
-                "./dist/js/modules/infragistics.ui.popover.js",
-                "./dist/js/modules/infragistics.ui.notifier.js",
-                "./dist/js/modules/infragistics.ui.editors.js",
-                "./dist/js/modules/infragistics.ui.tree.js",
-                "./dist/js/modules/infragistics.ui.layoutmanager.js",
-                "./dist/js/modules/infragistics.ui.splitter.js",
-                "./dist/js/modules/infragistics.ui.splitbutton.js",
-                "./dist/js/modules/infragistics.ui.rating.js",
-                "./dist/js/modules/infragistics.ui.toolbarbutton.js",
-                "./dist/js/modules/infragistics.ui.colorpicker.js",
-                "./dist/js/modules/infragistics.ui.colorpickersplitbutton.js",
-                "./dist/js/modules/infragistics.ui.toolbar.js",
-                "./dist/js/modules/infragistics.ui.tilemanager.js",
-                "./dist/js/modules/infragistics.ui.upload.js",
-                "./dist/js/modules/infragistics.ui.validator.js",
-                "./dist/js/modules/infragistics.ui.htmleditor.js",
-                "./dist/js/modules/infragistics.ui.videoplayer.js",
-                "./dist/js/modules/infragistics.ui.zoombar.js"
-            ]
+                // use the general "keep some" regexp  
+                preserveComments: /(?:^!|@(?:license|preserve|cc_on))/,
+                compress: false,
+                mangle: false
+            },
+            //files: buildLocaleMergePairs("en")
         }
     },
-    "mergeLocales": {
-        "files": buildLocaleMergePairs("en")
-    },
-    "all": {
-        files: [{
-          expand: true,
-          cwd: 'dist/',
-          src: ['**/*.js', '!**/*-lite.js'],
-          dest: 'dist/'
+    concat: {
+        dist: {
+            src: ['src/intro.js', 'src/project.js', 'src/outro.js'],
+            dest: 'dist/built.js',
+            files: {
+                "./dist/js/infragistics.core-lite.js": [
+                    "./dist/js/i18n/infragistics-en.js",
+                    "./dist/js/modules/infragistics.util.js",
+                    "./dist/js/modules/infragistics.dataSource.js",
+                    "./dist/js/modules/infragistics.templating.js",
+                    "./dist/js/modules/infragistics.ui.shared.js",
+                    "./dist/js/modules/infragistics.ui.scroll.js"
+                ],
+                "./dist/js/infragistics.lob-lite.js": [
+                    "./dist/js/modules/infragistics.ui.combo.js",
+                    "./dist/js/modules/infragistics.ui.dialog.js",
+                    "./dist/js/modules/infragistics.ui.popover.js",
+                    "./dist/js/modules/infragistics.ui.notifier.js",
+                    "./dist/js/modules/infragistics.ui.editors.js",
+                    "./dist/js/modules/infragistics.ui.tree.js",
+                    "./dist/js/modules/infragistics.ui.layoutmanager.js",
+                    "./dist/js/modules/infragistics.ui.splitter.js",
+                    "./dist/js/modules/infragistics.ui.splitbutton.js",
+                    "./dist/js/modules/infragistics.ui.rating.js",
+                    "./dist/js/modules/infragistics.ui.toolbarbutton.js",
+                    "./dist/js/modules/infragistics.ui.colorpicker.js",
+                    "./dist/js/modules/infragistics.ui.colorpickersplitbutton.js",
+                    "./dist/js/modules/infragistics.ui.toolbar.js",
+                    "./dist/js/modules/infragistics.ui.tilemanager.js",
+                    "./dist/js/modules/infragistics.ui.upload.js",
+                    "./dist/js/modules/infragistics.ui.validator.js",
+                    "./dist/js/modules/infragistics.ui.htmleditor.js",
+                    "./dist/js/modules/infragistics.ui.videoplayer.js",
+                    "./dist/js/modules/infragistics.ui.zoombar.js"
+                ]
+            }
         }
-      ]
     }
 };
 
 /**
  * Prepare bundles for each localization
  */
-for (var i = 0; i < locales.length; i++) {
+/*for (var i = 0; i < locales.length; i++) {
     newFiles["./dist/js/i18n/infragistics-" + locales[i] + ".js"] = "./dist/js/modules/i18n/*-" + locales[i] + ".js"; 
 }
 // keep order
 for (var key in module.exports.combine.files) {
      newFiles[key] = module.exports.combine.files[key];
 }
-module.exports.combine.files = newFiles;
+module.exports.combine.files = newFiles; */
