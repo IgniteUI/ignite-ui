44395b80af051ea267b7c452a17421f62e340530
Aleksandar Kamenov <AKamenov@infragistics.com>
2016-08-08 18:01:27 +0300

    Bug fix #219768

--- src/js/modules/infragistics.ui.htmleditor.js
+++ src/js/modules/infragistics.ui.htmleditor.js
@@ -2734,14 +2734,15 @@ if (typeof jQuery !== "function") {
                         this._updateSelection(range);
                     }
                 }
-
-                // A.K August 8th, 2016 Bug #219768 Toolbar button does not work properly for a selected content range if a 
-                // text is initially selected by double-tapping, and the selection is changed by mouse dragging.
-                // Fixed the bug with putting the code below in comment.
+        
                 // K.D. October 9th, 2012 Bug #115567 The browser version should be checked against >= 9 not === 9
-                // if (($.ig.util.isIE && $.ig.util.browserVersion >= 9) || $.ig.util.isOpera) {
-                //     this._updateSelection(range);
-                // }
+                if (($.ig.util.isIE && $.ig.util.browserVersion >= 9) || $.ig.util.isOpera) {
+                    // A.K August 8th, 2016 Bug #219768 Toolbar button does not work properly for a selected content range if a 
+                    // text is initially selected by double-tapping, and the selection is changed by mouse dragging.
+                    if(this._selection.focusNode.nodeType != 3){
+                        this._updateSelection(range);
+                    }
+                }
 
                 if ($.isFunction(customCommand) && browser === null) {
                     customCommand.call(this, name, args);
@@ -2946,7 +2947,12 @@ if (typeof jQuery !== "function") {
                     // This causes lost of the last chosen command when
                     // selecting multiple commands at once without clicking
                     // or typing something to trigger onSelectionChange
-                    this._updateSelection(this._range);
+                    
+                    // A.K August 8th, 2016 Bug #219768 Toolbar button does not work properly for a selected content range if a 
+                    // text is initially selected by double-tapping, and the selection is changed by mouse dragging.
+                    if(this._selection.focusNode.nodeType != 3){
+                        this._updateSelection(this._range);
+                    }
                 } else {
 
                     // Return the focus to the body
