f01b34488e5e4c2728ae8f03889cf17e1f498df3
Maya <MKirova@infragistics.com>
2016-09-10 15:01:24 +0300

    Adding test for internal bug 224258 with an additional fix
    Adding test for internal bug 224258 with an additional fix for scenario
    where remote groupBy and local paging is enabled.

--- src/js/modules/infragistics.datasource.js
+++ src/js/modules/infragistics.datasource.js
@@ -1749,6 +1749,12 @@
 					(!fApplied || s.defaultFields !== s.expressions) ) {
 					this.sort(s.defaultFields, s.defaultDirection);
 				}
+				/* M.H. 26 Aug 2016 Fix for bug 224258: Remote groupBy does not work in HierarchicalGrid */
+				if (!this._gbDataView && this.isGroupByApplied(this.settings.sorting.expressions)) {
+					this._generateGroupByData(this._filter ? this._filteredData :
+																this._data,
+											this.settings.sorting.expressions);
+				}
 				/* Check if paging is configured, and if so,
 				if OpType === $.ig.Constants.OpType.Local => apply local paging */
 				if (p.paging.enabled && p.paging.type === "local" && this._runtimeType !== "remoteUrl") {
@@ -1759,12 +1765,6 @@
 				entire data source after persisting filtering with data bind */
 				if (!this._filter || !this._transformedData) {
 					this._populateTransformedData();
-				}
-				/* M.H. 26 Aug 2016 Fix for bug 224258: Remote groupBy does not work in HierarchicalGrid */
-				if (!this._gbDataView && this.isGroupByApplied(this.settings.sorting.expressions)) {
-					this._generateGroupByData(this._filter ? this._filteredData :
-																this._data,
-											this.settings.sorting.expressions);
 				}
 				/* invoke the callback if present: */
 				if (this._runtimeType !== "remoteUrl") {
