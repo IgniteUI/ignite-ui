c8938510476a2b470c9e84915390a8d58a8d3b33
Alex Kartavov <AKartavov@infragistics.com>
2016-09-26 16:05:36 -0400

    added placeholders for AMD modules that are open sourced

--- src/js/modules/infragistics.datasource.js
+++ src/js/modules/infragistics.datasource.js
@@ -30,7 +30,7 @@
 	if (typeof define === "function" && define.amd) {
 
 		// AMD. Register as an anonymous module.
-		define( [
+		define( /*"igniteui/js/modules/infragistics.datasource",*/ [
 			"jquery",
 			"./infragistics.util"
 		], factory );
--- src/js/modules/infragistics.ui.tree.js
+++ src/js/modules/infragistics.ui.tree.js
@@ -23,7 +23,7 @@
 	if (typeof define === "function" && define.amd) {
 
 		// AMD. Register as an anonymous module.
-		define( [
+		define( /*"igniteui/js/modules/infragistics.ui.tree",*/ [
 			"jquery",
 			"jquery-ui",
 			"./infragistics.util",
