969af362d3cfa3d24e5d3fe6a5895e6d5fee6dba
Tsanna Belcheva <cannabelcheva90@gmail.com>
2017-01-19 17:48:49 +0200

    Fix for bug #229440
    When new rows are added to the grid while there are grouped columns and
    paging is enabled the rendered rows count does not match the total count
    displayed in the pager label

--- src/js/modules/infragistics.datasource.js
+++ src/js/modules/infragistics.datasource.js
@@ -3087,11 +3087,6 @@
 					this._commitTransaction(this._transactionLog.pop());
 				}
 			}
-			if (this.isGroupByApplied(this.settings.sorting.expressions)) {
-				this._generateGroupByData(this._filter ? this._filteredData :
-															this._data,
-										this.settings.sorting.expressions);
-			}
 		},
 		rollback: function (id) {
 			/* clears the transaction log without updating anything in the data source
