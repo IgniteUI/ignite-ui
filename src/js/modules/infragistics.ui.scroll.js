8ce748faa82fbbd3e951764abfa11d90f8ea84f0
skrustev <striker18@mail.bg>
2017-02-03 16:51:27 +0200

    Fix for bug 231326. On the latest Firefox when grid doesn't have scrollbars the page doesn not scroll when moving over the grid.

--- src/js/modules/infragistics.ui.scroll.js
+++ src/js/modules/infragistics.ui.scroll.js
@@ -2350,7 +2350,7 @@
 			}
 
 			//On Safari preventing the touchmove would prevent default page scroll behaviour even if there is the element doesn't have overflow
-			if (!$.ig.util.isSafari || ($.ig.util.isSafari && !this._igScollTouchPrevented)) {
+			if (!this._igScollTouchPrevented) {
 				event.preventDefault();
 			}
 		},
