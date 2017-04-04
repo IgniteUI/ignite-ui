2ae70b2fb46b6b3d321ef10d9f85765da397dabf
AMarinov <amarinov@infragistics.com>
2017-04-04 11:34:24 +0300

    Trailing whitespace removed

--- src/js/modules/infragistics.ui.editors.js
+++ src/js/modules/infragistics.ui.editors.js
@@ -1778,7 +1778,7 @@
 					}
 					break;
 				case "listItems":
-				
+
 					// A. M. April, 4th 2017 #921 "Cannot apply `listItems` to `igNumericEditor` after initialization"
 					// This is the same fix made on Nov 8, 2016 by M. S. for Issue #481 in version 17.1
 					if (prevValue !== null) {
