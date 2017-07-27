0b633529d562e927e58abb9f957915ffb54a6433
IvayloG <inj.iganchev@gmail.com>
2017-07-27 15:24:47 +0300

    Checking for textMode multiline and do not enter editing mode

--- src/js/modules/infragistics.ui.editors.js
+++ src/js/modules/infragistics.ui.editors.js
@@ -2834,10 +2834,12 @@
 							}
 						} else {
 							// We repeat the logic in case we don't have dropdown list. On enter the value is updated with the current value into editorInput.
-							this._processValueChanging(currentInputVal);
-
-							// A. M. 20/07/2016 #98 'Value of numeric editor is not set to 'minValue' after pressing ENTER'
-							this._enterEditMode();
+							this._processValueChanging(currentInputVal);							
+							//I.G. 20/07/2017 # 1090 'igTextEditor in multiline mode removes the existing text on Enter key'					
+							if (this.options.textMode !== "multiline") {
+							//A. M. 20/07/2016 #98 'Value of numeric editor is not set to 'minValue' after pressing ENTER'				
+								this._enterEditMode();
+							}
 						}
 					}
 				} else {
@@ -4799,7 +4801,7 @@
 					} else {
 
 						// We repeat the logic in case we don't have dropdown list. On enter the value is updated with the current value into editorInput
-						this._processValueChanging(currentInputVal);
+						this._processValueChanging(currentInputVal);				
 						this._enterEditMode();
 					}
 
