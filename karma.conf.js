0229f7f9724525374a6a983d5281a66b516f6eb3
Milko Venkov <mvenkov@infragistics.com>
2018-01-09 15:33:26 +0200

    Move editors/AriaRendering tests to new Karma/QUnit. Add appendToFixture method in test-util.js

--- karma.conf.js
+++ karma.conf.js
@@ -42,6 +42,7 @@ module.exports = function(config) {
       //'tests/unit/**/*test*.htm*'
       "tests/unit/colorpicker/tests.html",
       "tests/unit/editors/checkboxEditor/tests.html",
+      "tests/unit/editors/AriaRendering/tests.html",
       "tests/unit/colorpickersplitbutton/tests.html",
       "tests/unit/zoombar/tests.html",
       "tests/*-test.js"
--- tests/unit/common/test-util.js
+++ tests/unit/common/test-util.js
@@ -1,47 +1,49 @@
 (function ($) {
-
 	$.ig = $.ig || {};
 	$.ig.TestUtil = $.ig.TestUtil || {};
-	
-	$.extend( $.ig.TestUtil, {
-		
+
+	$.extend($.ig.TestUtil, {
+
 		// Use: $.ig.TestUtil.checkClass(elementToCheck, classToCheckFor)
 		checkClass: function (element, cls) {
-			if (QUnit && typeof(QUnit.ok) === 'function') {
+			if (QUnit && typeof (QUnit.ok) === 'function') {
 				QUnit.ok(element.hasClass(cls), 'The control with id: ' + element[0].id + ' does not contain the class: ' + cls);
 			} else {
 				return element.hasClass(cls);
 			}
 		},
-		
+
 		// Use: $.ig.TestUtil.getAbsolutePosition(elementToCheck)
 		getAbsolutePosition: function (element) {
-			var position = { X: 0, Y: 0 };
+			var position = {
+				X: 0,
+				Y: 0
+			};
 			var cElement = element;
-			
+
 			while (cElement !== null) {
 				position.X += cElement.offsetLeft;
 				position.Y += cElement.offsetTop;
-				
+
 				cElement = cElement.offsetParent;
 			}
-			
+
 			return position;
 		},
-		
+
 		// Use: $.ig.TestUtil.boolParse(param) : returns false with empty parameter
-		boolParse: function(result) {
+		boolParse: function (result) {
 			return !!result && result !== 'false';
 		},
 
 		/// Input interactions
-		
+
 		/**
 		 * Performs a paste on an input
 		 * @param input The HTMLInput element to paste into
 		 * @param newVal The text to paste in (at cursor position or over selection if focused)
 		 */
-		paste: 	function (input, newVal) {
+		paste: function (input, newVal) {
 			// create a mouse click event
 			var event = document.createEvent('Event'),
 				start = input.selectionStart,
@@ -52,7 +54,7 @@
 
 			// since ClipboardEvent constructor is not supported yet http://caniuse.com/#feat=clipboardData
 			// Also can't modify the event object in phantom, use IE's version
-			window.clipboardData = { 
+			window.clipboardData = {
 				getData: function (param) {
 					param = param.toLowerCase();
 					if (param === 'text/plain' || param === 'text') {
@@ -63,8 +65,8 @@
 
 			// NB! paste event is fired *before* any changes are applied to the DOM
 			input.dispatchEvent(event);
-			
-			if(!event.defaultPrevented) {
+
+			if (!event.defaultPrevented) {
 				// set value and cursor
 				oldVal = $(input).val();
 				prefix = oldVal.substring(0, start);
@@ -79,11 +81,12 @@
 		},
 
 		drop: function (input, newVal, focus) {
-			var ev = jQuery.Event( "drop" ), $input = $(input),
+			var ev = jQuery.Event("drop"),
+				$input = $(input),
 				startPos = $input[0].selectionStart,
 				focus = focus !== undefined ? focus : true;
 			ev.originalEvent = {
-				dataTransfer: { 
+				dataTransfer: {
 					getData: function (param) {
 						param = param.toLowerCase();
 						if (param === 'text/plain' || param === 'text') {
@@ -112,14 +115,14 @@
 		 */
 		wait: function (ms) {
 			if (typeof Promise !== "undefined") {
-				return new Promise(function(resolve) { 
+				return new Promise(function (resolve) {
 					setTimeout(resolve, ms)
 				});
 			} else {
 				// TODO: Only for Phantom, remove
 				var dfd = jQuery.Deferred();
 				dfd["catch"] = dfd.fail;
-				setTimeout(function() {
+				setTimeout(function () {
 					dfd.resolve();
 				}, ms);
 				return dfd;
@@ -150,9 +153,10 @@
 		keyInteraction: function (key, target, special) {
 			// could use an update in the future - https://www.w3.org/TR/DOM-Level-3-Events/#keypress-event-order
 			var char, startPos, endPos, newPos, textInput, prevented = false;
-			char = ( key > 31 &&
+			char = (key > 31 &&
 				(key < 37) || key > 40) //arrows
-				? String.fromCharCode(key) : "";
+				?
+				String.fromCharCode(key) : "";
 			if (special && char) {
 				char = special === "shiftKey" ? char.toUpperCase() : "";
 				key = char.charCodeAt(0);
@@ -193,7 +197,10 @@
 			var evt = $.Event("keydown");
 			evt.keyCode = key;
 			evt.charCode = key;
-			evt.originalEvent = { preventDefault: $.noop, stopPropagation: $.noop };
+			evt.originalEvent = {
+				preventDefault: $.noop,
+				stopPropagation: $.noop
+			};
 			if (special) {
 				evt[special] = true;
 				evt.originalEvent[special] = true;
@@ -232,8 +239,8 @@
 			if (target instanceof $) {
 				target = target[0];
 			}
-			params = $.extend({ 
-				"button": 0 ,
+			params = $.extend({
+				"button": 0,
 				"bubbles": true,
 				"cancelable": true
 			}, params);
@@ -254,13 +261,23 @@
 				this.mouseEvent(target, "mouseup", params);
 				this.mouseEvent(target, "click", params);
 			}
+		},
+		/**
+		 * Creates html element with provided options and adds it to qunit-fixture element
+		 * @param {string} element Html element, e.g. "<div></div>"
+		 * @param {object} options Optional object with value accepted by $(element, options) function
+		 * @returns Created element wrapped in jQuery object
+		 */
+		appendToFixture: function (element, options) {
+			var $qunitFixture = $('#qunit-fixture');
+			return $(element, options).appendTo($qunitFixture);
 		}
 	});
 
 	// patch ":focus" pseudo for PhantomJS 
-	$.expr[':'].focus = function( elem ) {
+	$.expr[':'].focus = function (elem) {
 		return elem === document.activeElement;
 	};
 	//must be called once:
-	 $(document).is(":focus");
+	$(document).is(":focus");
 }(jQuery));
