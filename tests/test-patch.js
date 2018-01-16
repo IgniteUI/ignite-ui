05de47acc2cf2b4eb9377c7fbd0ab5b486b63a02
Damyan Petev <damyan.petev@gmail.com>
2018-01-16 13:52:34 +0200

    Fixture cleanup, add debug tweaks

--- tests/test-patch.js
+++ tests/test-patch.js
@@ -1,9 +1,40 @@
-// This will run before html files
+/** Global fixture empty */
+QUnit.testDone( ( { module, name, total, passed, failed, skipped, todo, runtime } ) => {
+	// QUnit and adapter use `innerHTML`/`removeChild` on start
+	// Ensure proper destroy on controls after test:
+	$("#qunit-fixture").empty();
+});
 
-const start =  function (params) {
-	throw new Error("use assert.async()");
+/**
+ * Debug adjustments
+ */
+
+if (location.pathname === "/debug.html") {
+	QUnit.begin(function( details ) {
+		//console.log( "Test amount:", details.totalTests );
+		$("#qunit").css({float:"right", width: "50%", overflow:"auto"});
+	});
+
+
+	/** Mini Karma plugin, just override start to register QUnit handler after the karma adapter */
+	function createStartFn(karma, defaultConfig) {
+		var originalStart = karma.start;
+
+		return function () {
+			originalStart();
+			// This should run after karma-qunit `testStart` that recreates the fixture:
+			QUnit.testStart(function( details ) {
+				$("#qunit-fixture").css({
+					position: "static",
+					float:"left",
+					width: "50%",
+					overflow:"auto",
+					padding:"20px",
+					height:"100%",
+					boxSizing: "border-box"
+				});
+			});
+		}
+	}
+	window.__karma__.start = createStartFn(window.__karma__);
 }
-const stop = start;
-
-const module = QUnit.module;
-const test = QUnit.test;
