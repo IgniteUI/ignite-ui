e993e027122d4fd8b264339f2b1868246e717a41
NikolayAlipiev <nikolay.alipiev@gmail.com>
2018-02-14 11:01:06 +0200

    Exclude i18n folder from coverage report.

--- karma.conf.js
+++ karma.conf.js
@@ -152,7 +152,8 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      "src/**/*.js": "coverage"
+      "src/js/modules/*.js": "coverage",
+      "src/js/extensions/*.js": "coverage"
     },
 
     // https://github.com/karma-runner/karma-coverage/blob/master/docs/configuration.md
