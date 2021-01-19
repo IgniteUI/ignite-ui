183568cec24ae89a2521868ed735543c19fa6dfc
iganchev <iganchev@infragistics.com>
2021-01-19 13:50:47 +0200

    feat(localization): Add files structure for
    Italian, Chinese (Taiwan) zh-tw and Chinese (PRC) zh-cn

---
+++ src/js/modules/i18n/infragistics.ui.combo-it.js
@@ -0,0 +1,36 @@
+﻿/*!@license
+* Infragistics.Web.ClientUI Combo localization resources <build_number>
+*
+* Copyright (c) 2011-<year> Infragistics Inc.
+*
+* http://www.infragistics.com/
+*
+*/
+
+(function (factory) {
+	if (typeof define === "function" && define.amd) {
+		define( ["jquery"], factory );
+	} else {
+		return factory(jQuery);
+	}
+}
+(function ($) {
+	$.ig = $.ig || {};
+	$.ig.locale = $.ig.locale || {};
+	$.ig.locale.it = $.ig.locale.it || {};
+	$.ig.Combo = $.ig.Combo || {};
+	
+	$.ig.locale.it.Combo = {
+			noMatchFoundText: 'No matches found',
+			dropDownButtonTitle: 'Show drop-down',
+			clearButtonTitle: 'Clear value',
+			placeHolder: 'select...',
+			notSuported: 'Operation is not supported.',
+			errorNoSupportedTextsType: "A different filtering text is required. Provide a value that is either a string or an array of strings.",
+			errorUnrecognizedHighlightMatchesMode: "A different highlight matches mode is required. Choose a value between 'multi', 'contains', 'startsWith', 'full' and 'null'.",
+			errorIncorrectGroupingKey: "Grouping key is not correct."
+	};
+
+	$.ig.Combo.locale = $.ig.Combo.locale || $.ig.locale.it.Combo;
+	return $.ig.locale.it.Combo;
+}));// REMOVE_FROM_COMBINED_FILES
---
+++ src/js/modules/i18n/infragistics.ui.tree-it.js
@@ -0,0 +1,43 @@
+﻿/*!@license
+* Infragistics.Web.ClientUI Tree localization resources <build_number>
+*
+* Copyright (c) 2011-<year> Infragistics Inc.
+*
+* http://www.infragistics.com/
+*
+*/
+
+(function (factory) {
+	if (typeof define === "function" && define.amd) {
+		define( ["jquery"], factory );
+	} else {
+		return factory(jQuery);
+	}
+}
+(function ($) {
+	$.ig = $.ig || {};
+	$.ig.Tree = $.ig.Tree || {};
+	$.ig.locale = $.ig.locale || {};
+	$.ig.locale.it = $.ig.locale.it || {};
+
+	$.ig.locale.it.Tree = {
+			    invalidArgumentType: 'Invalid argument type provided.',
+			    errorOnRequest: 'An error has occurred while retrieving data: ',
+			    noDataSourceUrl: 'The igTree control requires a dataSourceUrl provided in order to initiate a request for data to that URL.',
+			    incorrectPath: 'A node was not found at the provided path: ',
+			    incorrectNodeObject: 'The provided argument is not a jQuery node element.',
+			    setOptionError: 'Runtime changes are not allowed for the following option: ',
+			    moveTo: '<strong>Move to</strong> {0}',
+			    moveBetween: '<strong>Move between</strong> {0} and {1}',
+			    moveAfter: '<strong>Move after</strong> {0}',
+			    moveBefore: '<strong>Move before</strong> {0}',
+			    copyTo: '<strong>Copy to</strong> {0}',
+			    copyBetween: '<strong>Copy between</strong> {0} and {1}',
+			    copyAfter: '<strong>Copy after</strong> {0}',
+			    copyBefore: '<strong>Copy before</strong> {0}',
+			    and: 'and'
+	}
+		
+	$.ig.Tree.locale = $.ig.Tree.locale || $.ig.locale.it.Tree;
+	return $.ig.locale.it.Tree;
+}));// REMOVE_FROM_COMBINED_FILES
