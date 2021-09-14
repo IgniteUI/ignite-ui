e068d3af7929ba4c23404488c6b1ae7cd5000159
iganchev <iganchev@infragistics.com>
2021-09-14 14:09:00 +0300

    feat(localization): Add structure for nb-NO

---
+++ src/js/modules/i18n/infragistics.shared-nb-NO.js
@@ -0,0 +1,28 @@
+﻿/*!@license
+* Infragistics.Web.ClientUI shared localization resources <build_number>
+*
+* Copyright (c) 2011-<year> Infragistics Inc.
+*
+* http://www.infragistics.com/
+*
+*/
+
+(function (factory) {
+	if (typeof define === "function" && define.amd) {
+		define( ["jquery"], factory );
+	} else {
+		return factory(jQuery);
+	}
+}
+(function ($) {
+	$.ig = $.ig || {};
+	$.ig.locale = $.ig.locale || {};
+	$.ig.locale['nb-NO'] = $.ig.locale['nb-NO'] || {};
+	$.ig.SharedLocale = $.ig.SharedLocale || {};
+	
+	$.ig.locale['nb-NO'].SharedLocale = {
+
+	};
+$.ig.SharedLocale.locale = $.ig.SharedLocale.locale || $.ig.locale['nb-NO'].SharedLocale;
+return $.ig.locale['nb-NO'].SharedLocale;
+}));// REMOVE_FROM_COMBINED_FILES
