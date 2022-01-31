524abdd3f0e4b60cc641086d7fd5ed274baf8c1e
VDyulgerov <volen1999@gmail.com>
2022-01-31 09:44:43 +0200

    fix bug #2169 list of open combos is fixed and does not scroll

--- src/js/modules/infragistics.ui.combo.js
+++ src/js/modules/infragistics.ui.combo.js
@@ -2588,6 +2588,9 @@
 				if (item.childElementCount > 0) {
 					this._setItemHTML(item.children, $listItem.children());
 				} else {
+					// V.D. 21th January 2022 Bug #2169 - If virtualization is TRUE and itemTemplate is set, the list of open combos is fixed and does not scroll
+					// https://github.com/IgniteUI/ignite-ui/issues/2169
+					$listItem[ 0 ].children[ i ].defaultValue = item.defaultValue;
 					$listItem[ 0 ].children[ i ].innerHTML = item.innerHTML;
 				}
 			}
